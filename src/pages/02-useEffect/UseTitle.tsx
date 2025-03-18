371d3145f42e6801d65186e7785aa770217df048
otwaylee <ahyeong858@gmail.com>
2025-03-19 01:00:28 +0900

    design: 불필요한 css 삭제

--- src/pages/02-useEffect/UseTitle.tsx
+++ src/pages/02-useEffect/UseTitle.tsx
@@ -4,7 +4,7 @@ const UseTitle = () => {
   const titleUpdater = useTitle('Loading...');
   setTimeout(() => titleUpdater('Home'), 3000);
   return (
-    <div className='App'>
+    <div>
       <div>Hi</div>
     </div>
   );
